351db4602093e017339a8676252cce7cd50eed03
JayaPrakash-rgb <jayaprakashrajana95@gmail.com>
2026-05-02 16:39:21 +0530

    app

---
+++ DAY2/app.js
@@ -0,0 +1,11 @@
+const login = () => {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if (username === "jp" && password === "123") {
+        alert("Login successful!");
+        window.location.href = "app.html";
+    } else {
+        alert("Invalid username or password.");
+    }
+}
