66a608a6444a2202b7b2053351186028b4999e21
JayaPrakash-rgb <jayaprakashrajana95@gmail.com>
2026-05-05 17:38:05 +0530

    apis prac

--- DAY4/in.js
+++ DAY4/in.js
@@ -15,3 +15,37 @@
  .then(data => data.map(d => (d.title = "here we go ->"+d.title)))
   .then(data=> console.log(data))
  .catch(error => console.log("error:",error));
+
+
+ // api-1
+
+fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
+.then(res => res.json())
+.then(obj => obj.results)
+.then(arr => arr.filter(p => p.name.startsWith("p")))
+.then(arr => arr.map(p => p.name))
+.then(console.log);
+
+//api-2
+fetch("https://dummyjson.com/products")
+.then(res =>res.json())
+.then(data => data.products)
+.then(data=> data.filter(item=> item.price>50))
+.then(data => console.log(data))
+
+
+//api -3 
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then(r => r.json())
+  .then(a => a.filter(p => p.userId === 1))
+  .then(a => a.map(p => p.title))
+  .then(console.log)
+  .catch(console.error);
+
+  //api -4
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
+.then(r => r.json())
+.then(obj => obj.results)
+.then(arr => arr.filter(p => p.name.startsWith("b")))
+.then(arr => arr.map(p => p.name))
+.then(console.log);
