1571c62628b0360bfae366a50f119c0d048b7735
JayaPrakash-rgb <jayaprakashrajana95@gmail.com>
2026-05-06 15:14:33 +0530

    oops concepts

--- DAY5/abstrac.js
+++ DAY5/abstrac.js
@@ -1,20 +1,37 @@
-class CoffeMachine {
-    #waterTemperature = 60;
- 
-    #boilWater() {
-        return `Boiling water to ${this.#waterTemperature} degrees.`;
+class coffeemachine {
+    #watertemp =60;
+
+    #boilwater()
+     { return `boil water at ${this.#watertemp} degrees`;
+
     }
- 
-    #grindBeans() {
-        return "Grinding beans.";
+    #grindbeans(){
+        return `grind beans`;
     }
- 
-    brew() {
-        const boilMessage = this.#boilWater();
-        const grindMessage = this.#grindBeans();
-        return `${boilMessage} ${grindMessage} Brewing coffee.`;
+    brew(){
+        const water=this.#boilwater();
+        const beans=this.#grindbeans();
+        return `${water} ${beans} brewing is done`;
     }
 }
- 
-const myCoffeMachine = new CoffeMachine();
-console.log(myCoffeMachine.brew());
+const mycoffee = new coffeemachine;
+console.log(mycoffee.brew());
+
+////////// my example
+class mangojuice{
+    #mango="yellow mangos";
+
+#fruit(){
+    return `collect ${this.#mango} and cut in to pices `;
+}
+#ice(){
+    return `add sufficent ice and sugar`;
+}
+blend(){
+    const selectfruit = this.#fruit();
+    const process =this.#ice();
+    return `${selectfruit} ${process} blend for your mangojuice `;
+    }
+}
+const my= new  mangojuice;
+console.log(my.blend());
