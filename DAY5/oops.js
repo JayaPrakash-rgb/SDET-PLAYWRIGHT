5b260999c8168a7bd2f7074d60aa42aa08f3f2d1
JayaPrakash-rgb <jayaprakashrajana95@gmail.com>
2026-05-06 12:09:58 +0530

    oops

---
+++ DAY5/oops.js
@@ -0,0 +1,31 @@
+class Animal 
+{
+    constructor(name) 
+    {
+        this.name = name;
+    }
+
+    speak()
+     {
+        console.log(`${this.name} makes a sound.`);
+    }
+}
+ 
+const dog = new Animal("Jhonny");
+const cat = new Animal("Whiskers");
+dog.speak();
+cat.speak();
+
+
+class car{
+    constructor( brand){
+this.brand = brand;
+    }
+    performance(){
+        console.log(`${this.brand} mad performance.`);
+    }
+}
+const wheel=new car("TATA");
+const beast=new car("mahinddra");
+wheel.performance();
+beast.performance();
