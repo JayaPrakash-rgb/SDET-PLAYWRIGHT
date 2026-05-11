2246d56520fb3be4c9c115c233afc8b4ca077624
JayaPrakash-rgb <jayaprakashrajana95@gmail.com>
2026-05-11 12:49:03 +0530

    typescript basics

--- DAY9/index.ts
+++ DAY9/index.ts
@@ -1,2 +1,18 @@
-let name ="prakash";
-console.log(name);
+let name1 ="prakash";
+console.log(name1);
+console.log();
+
+let name: string = "prakash";
+let result: number | string;
+result = 20;
+result = "success";
+console.log(name, result);
+ console.log();
+
+
+function add(a: number, b: number): string
+{
+    return "a + b =  " + (a + b);
+}
+ 
+console.log(add(5, 10));
