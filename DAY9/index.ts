b39a96ca986c3ebca8bf216c35b529db14cf2fb8
JayaPrakash-rgb <jayaprakashrajana95@gmail.com>
2026-05-11 13:09:02 +0530

    typescript basics

--- DAY9/index.ts
+++ DAY9/index.ts
@@ -2,6 +2,7 @@ let name1 ="prakash";
 console.log(name1);
 console.log();
 
+
 let name: string = "prakash";
 let result: number | string;
 result = 20;
@@ -9,10 +10,15 @@ result = "success";
 console.log(name, result);
  console.log();
 
-
+// trying function in typescript
 function add(a: number, b: number): string
 {
     return "a + b =  " + (a + b);
 }
  
 console.log(add(5, 10));
+console.log()
+
+let res: number | string ;
+res = "a"
+console.log(res);
